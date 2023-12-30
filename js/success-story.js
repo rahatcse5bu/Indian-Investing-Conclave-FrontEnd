1d10836d4d6e02953abaacc47a0c00dbed040ed4
Md. Rahat <rahat.cse5.bu@gmail.com>
2023-12-30 14:35:36 +0600

    some issues fixed

--- js/success-story.js
+++ js/success-story.js
@@ -1,13 +1,13 @@
 
 $(document).ready(function(){
-        $('.my-footer-slick').slick({
-            slidesToShow: 1, // Number of visible slides
-            slidesToScroll: 1, // Number of slides to scroll
-            autoplay: true, // Auto-play slides
-            autoplaySpeed: 3000, // Auto-play speed in milliseconds (optional)
-            prevArrow: $('.slick-prev'), // Use the classes of the buttons as selectors
-            nextArrow: $('.slick-next'), // Use the classes of the buttons as selectors
-            // Add more configuration options as needed
-        });
+        // $('.my-footer-slick').slick({
+        //     slidesToShow: 1, // Number of visible slides
+        //     slidesToScroll: 1, // Number of slides to scroll
+        //     autoplay: true, // Auto-play slides
+        //     autoplaySpeed: 3000, // Auto-play speed in milliseconds (optional)
+        //     prevArrow: $('.slick-prev'), // Use the classes of the buttons as selectors
+        //     nextArrow: $('.slick-next'), // Use the classes of the buttons as selectors
+        //     // Add more configuration options as needed
+        // });
     });
     
