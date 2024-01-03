55d0a5079d751d2681dd74546caf69a62d3344a6
Md. Rahat <rahat.cse5.bu@gmail.com>
2024-01-03 10:42:56 +0600

    market cap changes onclick tab implemneted

--- js/home/filter-modal.js
+++ js/home/filter-modal.js
@@ -16,3 +16,20 @@
         });
 
         //end of modal 
+
+
+        // This function will be called whenever a market-cap element is clicked
+function toggleActiveClass(event) {
+    // First, remove the bg-primaryColor class from all elements
+    document.querySelectorAll('.market-cap').forEach(function(el) {
+        el.classList.remove('bg-primaryColor');
+    });
+  
+    // Then, add the bg-primaryColor class to the clicked element
+    event.currentTarget.classList.add('bg-primaryColor');
+}
+
+// Add click event listener to each market-cap element
+document.querySelectorAll('.market-cap').forEach(function(el) {
+    el.addEventListener('click', toggleActiveClass);
+});
